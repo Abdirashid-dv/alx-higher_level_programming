a47dd7bd02ccf70ae0f86715b9ca028fd8c43575
Abdirashidv <Cabsho123@gmail.com>
2024-04-08 02:47:41 -0700

    Updated 1-rectangel file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ Defines a class Rectangle
 class Rectangle:
     def __init__(self, width=0, height=0):
         """Initialize a Rectangle instance with optional width and height."""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
