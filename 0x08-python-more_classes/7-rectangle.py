cd742d0ef2fd50fed7ea4543da2ed81ef3af1435
Abdirashidv <Cabsho123@gmail.com>
2024-04-08 16:35:59 -0700

    Added task#6 & #7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,9 +7,11 @@ class Rectangle:
     
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
+        print_symbol (str) : The symbol used for string representation.
     """
 
     number_of_instances = 0
+    print_symbolc = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a Rectangle instance.
@@ -70,7 +72,7 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
@@ -85,5 +87,4 @@ class Rectangle:
         """Print a message for every deletion of a Rectangle."""
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
-
 
